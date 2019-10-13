f44646c42b7c01ec60b769993f878c45628c761e
Krause <leonhard-philipp-hagen.krause@capgemini.com>
2019-10-13 14:04:24 +0200

    main page component added

--- JamSessionClient/src/app/app.module.ts
+++ JamSessionClient/src/app/app.module.ts
@@ -41,10 +41,12 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
