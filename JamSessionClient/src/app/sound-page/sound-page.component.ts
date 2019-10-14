b5a15c37d3070a7d926bf2c9eb46881e54f9b94e
Krause <leonhard-philipp-hagen.krause@capgemini.com>
2019-10-14 13:22:39 +0200

    little fixes and sound page reworked

--- JamSessionClient/src/app/sound-page/sound-page.component.ts
+++ JamSessionClient/src/app/sound-page/sound-page.component.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Sounds {
+  name: string;
+  src: string;
+}
+
 @Component({
   selector: 'app-sound-page',
   templateUrl: './sound-page.component.html',
   styleUrls: ['./sound-page.component.css']
 })
+
 export class SoundPageComponent implements OnInit {
 
   constructor() { }
 
+  allSounds: Sounds [] = [
+    {name: 'Long', src: '../../assets/2.mp3'},
+    {name: 'Short', src: '../../assets/1.mp3'},
+  ];
+
   ngOnInit() {
   }
 
-  playSound1() {
+  playSound(src: string) {
     const audio = new Audio();
-    audio.src = '../../assets/2.mp3';
-    audio.load();
-    audio.play();
-  }
-
-  playSound2() {
-    const audio = new Audio();
-    audio.src = '../../assets/1.mp3';
+    audio.src = src;
     audio.load();
     audio.play();
   }
