43ef5d1ce6c7127b5f58c8bb9a9bdb2a965b1286
lheubach <lukas.heubach@gmx.de>
2019-10-29 14:30:10 +0100

    web socket connection fix

--- JamSessionClient/src/app/create-dialog/create-dialog.component.ts
+++ JamSessionClient/src/app/create-dialog/create-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 
 
 @Component({
@@ -9,26 +10,38 @@ import * as SockJS from 'sockjs-client';
 })
 export class CreateDialogComponent implements OnInit {
 
+  webSocket: WebSocket;
+  client: Stomp.Client;
+  sessionId: string;
+  playername: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   createSession() {
-    const playername = 'play';
-    const sessionId = 'sessionNaddsdme';
+    this.playername = 'play';
+    this.sessionId = 'sessionNaddsdme';
     // hier muss noch ne hash funktion rein
     const passwordHash = 'tesmp';
-    this.setCookie('name', playername);
-    this.setCookie('sessionId', sessionId);
+    this.setCookie('name', this.playername);
+    this.setCookie('sessionId', this.sessionId);
     this.setCookie('sessionPasswordHash', passwordHash);
-    const socket = new SockJS('ws://localhost:8080/ws');
+    this.webSocket = new WebSocket('ws://localhost:8080/ws');
 
+    this.client = Stomp.over(this.webSocket);
 
-    const stompClient = Stomp.over(socket);
+    this.client.connect({}, () => {
+      this.client.send('/app/jamsession/' + + '/addUser',
+      {},
+      JSON.stringify({sender: this.playername, type: 'JOIN'})
+      );
+      this.client.subscribe('/jamsession/' + this.sessionId, (item) => {
+        // TODO
+      });
+    });
 
-    stompClient.connect({}, null, null);
-    //  event.preventDefault();
   }
 
   private setCookie(name: string, val: string) {
@@ -38,6 +51,13 @@ export class CreateDialogComponent implements OnInit {
     date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
 
     document.cookie = name + '=' + value + '; expires=' + date.toUTCString() + '; path=/';
-}
+  }
+
+  test1() {
+
+  }
+  test2() {
+
+  }
 
 }
