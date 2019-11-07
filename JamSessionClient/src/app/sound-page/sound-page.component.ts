123dea29b861da999394c7a4a6e1d748c8b8ac2c
Krause <leonhard-philipp-hagen.krause@capgemini.com>
2019-11-07 14:49:14 +0100

    merge

--- JamSessionClient/src/app/sound-page/sound-page.component.ts
+++ JamSessionClient/src/app/sound-page/sound-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpClientService } from '../http-client/httpClient.service';
+import { SoundInterface } from '../http-client/SoundInterface';
 
 
 export interface Sounds {
@@ -19,6 +20,14 @@ export class SoundPageComponent extends HttpClientService implements OnInit {
   constructor(private http2: HttpClient) {
     super(http2);
   }
+
+  allAudioFiles: SoundInterface[] = [
+    {instrumentType: 'BANJO', pitchType: 'C_1', effect: 'NONE', data: null},
+    {instrumentType: 'BANJO', pitchType: 'C_3', effect: 'NONE', data: null},
+    {instrumentType: 'BANJO', pitchType: 'CIS_DES_4', effect: 'NONE', data: null},
+    {instrumentType: 'BANJO', pitchType: 'FIS_GES_5', effect: 'NONE', data: null},
+    {instrumentType: 'BANJO', pitchType: 'AIS_B_5', effect: 'NONE', data: null},
+  ];
 
   allSounds: Sounds [] = [
     {name: 'Long', src: '../../assets/2.mp3'},
